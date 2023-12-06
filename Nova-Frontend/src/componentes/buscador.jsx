7d5729cdea2172f093aa3b3607ca59d929f68a6c
CandelaValente <candevalente18@gmail.com>
2023-12-05 21:43:12 -0300

    .

---
+++ Nova-Frontend/src/componentes/buscador.jsx
@@ -0,0 +1,12 @@
+import React
+ from "react";
+
+export function Buscador() {
+
+    return (
+        <><div className="inputForm">
+            <input  type="text" placeholder="BuscarCurso"></input>
+        </div>
+        </>
+    );
+}
